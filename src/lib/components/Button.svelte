c742bfb32471b4ec8623d7e3944f5a94dba18a0b
DarviL82 <davidlosantos89@gmail.com>
2023-09-26 14:41:06 +0200

    add: button

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+	export let primary = false
+</script>
+
+<button class:primary>
+	<slot />
+</button>
+
+<style lang="scss">
+	.primary {
+		$glow-blur: 3rem;
+		$glow-opacity: 20%;
+
+		padding: 0.5rem 3rem;
+		background-image: linear-gradient(
+			45deg,
+			var(--color-accent-1),
+			var(--color-accent-2)
+		);
+		background-size: 200%;
+		color: var(--color-secondary);
+		font-weight: 900;
+		font-size: 3em;
+		border-radius: 10px;
+		box-shadow: 0 1rem $glow-blur 0
+				color-mix(
+					in srgb,
+					var(--color-accent-1) $glow-opacity,
+					rgba(0 0 0 / 0)
+				),
+			0 -1rem $glow-blur 0 color-mix(in srgb, var(--color-accent-2)
+						$glow-opacity, rgba(0 0 0 / 0));
+		outline: var(--color-accent-2) solid 0;
+		transition: outline-width 0.15s, outline-offset 0.15s, box-shadow 0.25s,
+			background-position 0.5s;
+
+		&:hover {
+			outline-width: 0.25rem;
+			outline-offset: 0.25rem;
+			--glow-opacity: 50%;
+			cursor: pointer;
+			background-position: right;
+			box-shadow: 0 0 4rem 0
+				color-mix(in srgb, var(--color-accent-2) 50%, rgba(0 0 0 / 0));
+		}
+	}
+</style>
--- src/routes/(components)/Banner.svelte
+++ src/routes/(components)/Banner.svelte
@@ -1,25 +1,33 @@
 <script lang="ts">
+	import Button from "$lib/components/Button.svelte"
 	import Image from "$lib/components/Image.svelte"
 	import logo_white from "$lib/images/logo/logo_white.png"
 </script>
 
 <div class="banner">
-	<Image src={logo_white} alt="Logo" />
+	<Image src={logo_white} alt="Logo" maxWidth={"30rem"} />
 	<p class="info">
 		Lanat is a command line argument parser for Java 17 with ease of use and
 		high customization possibilities in mind.
 	</p>
+	<Button primary>Get it now</Button>
 </div>
 
 <style lang="scss">
 	.banner {
 		background-color: var(--color-secondary);
 		height: 100%;
-		padding: 2rem;
+		padding: 4rem;
 		border-radius: var(--border-radius);
 		display: flex;
 		flex-direction: column;
 		align-items: center;
 		justify-content: center;
+		gap: 2.5rem;
+	}
+
+	.info {
+		text-align: center;
+		font-size: 1.25em;
 	}
 </style>
