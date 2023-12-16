a49a0a0458d57319decd6ff3ea8c15d143658625
darvil82 <davidlosantos89@gmail.com>
2023-12-16 17:56:28 +0100

    fix sizes on bigger screens

--- src/lib/components/Code.svelte
+++ src/lib/components/Code.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
+	import type { CSSUnit } from "$lib/typedefs"
 	import { Highlight } from "svelte-highlight"
 	import { java, type LanguageType } from "svelte-highlight/languages"
 
 	export let code: string
 	export let language: LanguageType<string> = java
+	export let fontSize: CSSUnit = "0.85rem"
 </script>
 
-<div class="code-wrapper">
+<div class="code-wrapper" style:font-size={fontSize}>
 	<Highlight {language} code={code.trim()} />
 </div>
 
@@ -17,7 +19,6 @@
 		background-color: var(--color-black);
 		height: 100%;
 		padding: 0.5rem;
-		font-size: 0.85rem;
 		border-radius: var(--border-radius);
 	}
 </style>
