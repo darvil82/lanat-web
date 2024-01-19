b190e195727f695efd92132fa404e0740ad06b48
darvil82 <davidlosantos89@gmail.com>
2024-01-19 14:38:04 +0100

    add opengraph

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,11 +4,28 @@
 	import Footer from "./(components)/Footer.svelte"
 	import "./app.scss"
 	import "svelte-highlight/styles/github-dark.css"
+	import { page } from "$app/stores"
+	import logo from "$lib/images/logo.png"
 
 	onMount(() => {
 		accentTheme.set(Math.floor(Math.random() * 3))
 	})
+
+	const description =
+		"A command line argument parser for Java 17 with ease of use and high customization possibilities in mind."
 </script>
+
+<svelte:head>
+	<title>Lanat</title>
+	<meta property="og:title" content="Lanat" />
+	<meta name="description" content={description} />
+	<meta property="og:description" content={description} />
+	<meta property="og:site_name" content="Lanat" />
+	<meta property="og:url" content={$page.url.href} />
+	<meta property="og:image" content={logo} />
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta name="theme-color" content={"#70ddc9"} />
+</svelte:head>
 
 <div class="main-wrapper accent-theme-{$accentTheme}">
 	<slot />
